a74a67abc50195cdb52f7751a7da919d5c6549a6
AlejoCasti <acastiblanco8@gmail.com>
2020-12-02 23:31:59 -0500

    fix

--- 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -2,8 +2,8 @@
 
 class Array {
     public static int[] ReplaceElement(int[] array, int index, int n) {
-        if (array.Length < index) {
-            Console.Write("Index out of range");
+         if (index >= array.Length || index < 0){
+            Console.WriteLine("Index out of range");
             return array;
         }
         array[index] = n;
